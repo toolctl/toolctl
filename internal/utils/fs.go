12f074dcae263e59b54d26132d6e01ca95003ea4
André Sá <enkodr@outlook.com>
2022-06-06 21:51:51 +0100

    Fix install command in mounted volumes on containers (#84)

---
+++ internal/utils/fs.go
@@ -0,0 +1,57 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// CopyFile copies the file from the source to the destination
+func CopyFile(src, dest string) error {
+	// open source file
+	srcFile, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("error opening source file: %s", err)
+	}
+	defer srcFile.Close()
+
+	// open destination file
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return fmt.Errorf("error opening destination file: %s", err)
+	}
+	defer destFile.Close()
+
+	// copy file
+	_, err = io.Copy(destFile, srcFile)
+	if err != nil {
+		return fmt.Errorf("writing to destination file failed: %s", err)
+	}
+
+	return nil
+}
+
+// MoveFile moves the file from the source to the destination
+func MoveFile(src, dest string) error {
+	// copy file
+	err := CopyFile(src, dest)
+	if err != nil {
+		return fmt.Errorf("failed copying file: %s", err)
+	}
+
+	// delete source file
+	err = os.Remove(src)
+	if err != nil {
+		return fmt.Errorf("failed removing original file: %s", err)
+	}
+	return nil
+}
+
+// SetPermissions sets a file permissions to be executable
+func SetPermissions(src string) error {
+	err := os.Chmod(src, 0755)
+	if err != nil {
+		return fmt.Errorf("failed changing file permissions: %s", err)
+	}
+	return nil
+}
---
+++ internal/utils/fs_test.go
@@ -0,0 +1,124 @@
+package utils_test
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/toolctl/toolctl/internal/utils"
+)
+
+var (
+	content = "this is a dummy file"
+)
+
+func createOriginFile() (*os.File, error) {
+	file, err := os.CreateTemp("/tmp", "toolctl_origin_")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	_, err = file.WriteString(content)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+func createEmptyfile() (*os.File, error) {
+	file, err := os.CreateTemp("/tmp", "toolctl_dest_")
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return file, nil
+
+}
+
+// TestCopyFile tests if a file is correctly copied from source to destination
+func TestCopyFile(t *testing.T) {
+	// Create origin file
+	sourceFilePath, err := createOriginFile()
+	if err != nil {
+		t.Errorf("failed to create the source file")
+	}
+	defer os.Remove(sourceFilePath.Name())
+
+	// Create destination file and copy content from the original file
+	destFilePath, _ := createEmptyfile()
+	err = utils.CopyFile(sourceFilePath.Name(), destFilePath.Name())
+	if err != nil {
+		t.Errorf("failed to copy file to destination")
+	}
+	defer os.Remove(destFilePath.Name())
+
+	// Read destination file content
+	destFileContent, err := ioutil.ReadFile(destFilePath.Name())
+	if err != nil {
+		t.Errorf("failed to open destination file")
+	}
+
+	// Compare the two files content
+	if strings.Compare(content, string(destFileContent)) != 0 {
+		t.Errorf("copied file content doesn't match the original")
+	}
+}
+
+// TestMoveFile tests if a file is correctly moved from source to destination
+func TestMoveFile(t *testing.T) {
+	// Create origin file
+	sourceFilePath, err := createOriginFile()
+	if err != nil {
+		t.Errorf("failed to create the source file")
+	}
+	defer os.Remove(sourceFilePath.Name())
+
+	// Create destination file
+	destFilePath, _ := createEmptyfile()
+
+	// Move the original file to the destination path
+	err = utils.MoveFile(sourceFilePath.Name(), destFilePath.Name())
+	if err != nil {
+		t.Errorf("failed to move file to destination")
+	}
+
+	// Check if the original file was removed
+	if _, err := os.Stat(sourceFilePath.Name()); os.IsExist(err) {
+		t.Error("original file was note removed")
+	}
+
+	// Read destination file content
+	destFileContent, err := ioutil.ReadFile(destFilePath.Name())
+	if err != nil {
+		t.Errorf("failed to open destination file")
+	}
+
+	// Compare the two files content
+	if strings.Compare(content, string(destFileContent)) != 0 {
+		t.Errorf("copied file content doesn't match the original")
+	}
+}
+
+func TestSetPermissions(t *testing.T) {
+	file, err := createEmptyfile()
+	if err != nil {
+		t.Errorf("failed to create the file")
+	}
+	err = utils.SetPermissions(file.Name())
+	if err != nil {
+		t.Errorf("failed to set file permissions")
+	}
+
+	stats, err := os.Stat(file.Name())
+	if err != nil {
+		t.Errorf("failed to read file permissions")
+	}
+
+	if stats.Mode() != 0755 {
+		t.Errorf("permissions not set correctly")
+	}
+}
